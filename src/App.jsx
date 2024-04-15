20df8dab858b0ab150bd1c6121492ebdd621d0c4
Patrik <patrik.hyytiainen@protonmail.com>
2024-04-15 11:14:11 +0300

    Complete first assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css';
+import './components/Home';
+import Home from './components/Home';
 const App = () => {
   return (
     <>
       <h1>My App</h1>
+      <Home />
     </>
   );
 };
