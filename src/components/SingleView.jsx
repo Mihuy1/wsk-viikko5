d4df7b2ddebdf9583beb1b88a990189715fe13de
Patrik <patrik.hyytiainen@protonmail.com>
2024-04-15 13:45:21 +0300

    Add a list of items and a button to open in dialog which shows image/video

---
+++ src/components/SingleView.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+function SingleView(props) {
+  const {item, isDialogOpen, setIsDialogOpen} = props;
+  return (
+    <dialog open={isDialogOpen}>
+      <h2>{item && item.title}</h2>
+      {item && item.media_type.startsWith('image') ? (
+        <img src={item.filename} alt={item.title} />
+      ) : (
+        <video controls src={item.filename} title={item.title}></video>
+      )}
+      <p>Created at: {item && item.created_at}</p>
+      <p>{item && item.description}</p>
+      <button onClick={() => setIsDialogOpen(false)}>Close</button>
+    </dialog>
+  );
+}
+
+SingleView.propTypes = {
+  item: PropTypes.object,
+  isDialogOpen: PropTypes.bool.isRequired,
+  setIsDialogOpen: PropTypes.func.isRequired,
+};
+
+export default SingleView;
