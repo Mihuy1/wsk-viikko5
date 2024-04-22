a2f9db86a6e17b76ee5fab0ca1ec8b40c377364a
Patrik <patrik.hyytiainen@protonmail.com>
2024-04-22 10:17:45 +0300

    Complete lab assignment 1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~patrikhy/wsk-routing/',
+  base: '/~patrikhy/wsk-hook/',
 });
